dde77fe3b2575f12efe2f38462c69ce3b81b3630
Neroil <edwin.haffner@heig-vd.ch>
2024-04-28 10:12:37 +0200

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,131 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        AttackHandler physicalAttackHandler = new PhysicalAttackHandler();
+        AttackHandler magicalAttackHandler = new MagicalAttackHandler(physicalAttackHandler);
+
+        Person[] fightingRing = new Person[3];
+
+        Person Gimli = new Person(physicalAttackHandler);
+        Mage Saruman = new Mage(magicalAttackHandler);
+
+        fightingRing[0] = Gimli;
+        fightingRing[2] = Saruman;
+
+        Gimli.attack(Saruman); //PhysicalAttack request, will fail
+        Saruman.attack(Gimli); //MagicalAttack request
+
+
+        fightingRing[1] = Gimli;
+    }
+}
+
+enum Kind {
+    MagicalAttack,
+    PhysicalAttack
+}
+
+class Request {
+
+    private Kind requestKind;
+
+    public Request(Kind resquestKind){
+        this.requestKind = resquestKind;
+    }
+
+    public Kind getKind() {
+        return requestKind;
+    }
+}
+
+
+abstract class AttackHandler {
+    protected AttackHandler _successor;
+
+    public AttackHandler(AttackHandler _successor){
+        this._successor = _successor;
+    }
+
+    public AttackHandler(){
+        this._successor = null;
+    }
+
+    public void handleAttackRequest(Request request){
+        this._successor.handleAttackRequest(request);
+    }
+}
+
+class PhysicalAttackHandler extends AttackHandler {
+
+    public PhysicalAttackHandler(AttackHandler _successor){
+        super(_successor);
+    }
+
+    public PhysicalAttackHandler(){
+        super();
+    }
+
+    @Override
+    public void handleAttackRequest(Request request) {
+        if (request.getKind() == Kind.PhysicalAttack) {
+            // Handle the request here
+        } else if (_successor != null) {
+            _successor.handleAttackRequest(request);
+        }
+    }
+}
+
+class MagicalAttackHandler extends AttackHandler {
+
+    public MagicalAttackHandler(AttackHandler _successor){
+        super(_successor);
+    }
+
+    public MagicalAttackHandler(){
+        super();
+    }
+
+    @Override
+    public void handleAttackRequest(Request request) {
+        if (request.getKind() == Kind.MagicalAttack) {
+            // Handle the request here
+        } else if (_successor != null) {
+            _successor.handleAttackRequest(request);
+        }
+    }
+}
+
+class Person {
+    protected AttackHandler handler;
+
+    public Person(AttackHandler handler) {
+        this.handler = handler;
+    }
+
+    public int getMana() {
+        return 0;
+    }
+
+    public void attack(Person p){
+        Request request = new Request(Kind.PhysicalAttack);
+        handler.handleAttackRequest(request);
+    }
+}
+
+class Mage extends Person{
+    public Mage(AttackHandler handler) {
+        super(handler);
+    }
+
+    @Override
+    public int getMana() {
+        return 100;
+    }
+
+    @Override
+    public void attack(Person p){
+        Request request = new Request(Kind.MagicalAttack);
+        handler.handleAttackRequest(request);
+    }
+}
